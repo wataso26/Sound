42b820a0d8bc58822f226b0abb263cfaa09d2304
lifeistech <2wata2wata2@gmail.com>
2021-05-01 11:42:43 +0900

    サウンドアプリ2完成

--- app/src/main/java/app/wataso_/watanabe/sound/MainActivity.kt
+++ app/src/main/java/app/wataso_/watanabe/sound/MainActivity.kt
@@ -5,20 +5,33 @@ package app.wataso_.watanabe.sound
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import kotlinx.android.synthetic.main.activity_main.*
+import java.security.KeyStore
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val drumSound =MediaPlayer.create(this,R.raw.drum_sound)
+        val drumSound = MediaPlayer.create(this, R.raw.drum_sound)
 
-        drumImage.setOnClickListener{
-            //ドラムの巻き戻しを意味する
-            drumSound.seekTo( 0)
-            //ドラムの音を再生する
-            drumSound.start()
+        drumImage.setOnTouchListener { v, event ->
+
+            if (event.action ==MotionEvent.ACTION_DOWN){
+
+                drumImage.setImageResource(R.drawable.drum_playing_image)
+
+                drumSound.seekTo(0)
+
+                drumSound.start()
+            }
+            else if (event.action ==MotionEvent.ACTION_UP){
+
+                drumImage.setImageResource(R.drawable.drum_image)
+            }
+
+            true
         }
     }
 }
